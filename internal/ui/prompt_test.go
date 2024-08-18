5a6fa7b0ac6097c68afb28b5108e629d5c90fece
coding-hui <wecoding@yeah.net>
2024-08-18 11:04:13 +0800

    refactor: update Assistant Name to ai in Prompts
    - Change the name from `Yai` to `ai` in the system prompt definition.
    - Update the example response in the system prompt to use `ai` instead of `Yai`.
    
    Signed-off-by: coding-hui <wecoding@yeah.net>

--- internal/ui/prompt_test.go
+++ internal/ui/prompt_test.go
@@ -10,13 +10,13 @@ func TestUIPrompt(t *testing.T) {
 }
 
 func testPrepareSystemPrompt(t *testing.T) {
-	a := "You are Yai a powerful terminal assistant created by github.com/coding-hui.\n" +
+	a := "You are ai a powerful terminal assistant created by github.com/coding-hui.\n" +
 		"You will answer in the most helpful possible way.\n" +
 		"Always format your answer in markdown format.\n\n" +
 		"For example:\n" +
 		"Me: What is 2+2 ?\n" +
-		"Yai: The answer for `2+2` is `4`\n" +
+		"ai: The answer for `2+2` is `4`\n" +
 		"Me: +2 again ?\n" +
-		"Yai: The answer is `6`\n"
+		"ai: The answer is `6`\n"
 	fmt.Println(a)
 }
