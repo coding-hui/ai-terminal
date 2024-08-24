bca418d57b1d2d747b29838c07d5ad2d9c205519
coding-hui <wecoding@yeah.net>
2024-08-24 22:56:17 +0800

    refactor: refactor test suite for clarity and maintainability
    - Remove unused imports and functions from test file
    - Simplify the `TestCommands` function by removing detailed test setup
    
    Signed-off-by: coding-hui <wecoding@yeah.net>

--- internal/coders/commands_test.go
+++ internal/coders/commands_test.go
@@ -3,35 +3,9 @@ package coders
 import (
 	"testing"
 
-	"github.com/spf13/viper"
-
-	"github.com/coding-hui/ai-terminal/internal/cli/options"
-
 	_ "github.com/coding-hui/ai-terminal/internal/session/mongo"
 	_ "github.com/coding-hui/ai-terminal/internal/session/simple"
 )
 
 func TestCommands(t *testing.T) {
-	t.Run("codingCmd", testCodingCmd)
-}
-
-func testCodingCmd(t *testing.T) {
-	cmds := prepareTestCommands()
-
-	cmds.addFiles("internal/coders/code_editor.go")
-
-}
-
-func prepareTestCommands() *command {
-	options.LoadConfig(viper.GetString(options.FlagAIConfig), "ai")
-	coder := NewAutoCoder()
-	coder.Init()
-
-	// mock status
-	select {
-	case <-coder.checkpointChan:
-	default:
-	}
-
-	return newCommand(coder)
 }
