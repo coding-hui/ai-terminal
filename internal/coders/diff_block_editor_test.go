a221a54d00c5a0ff1b1eac3aff107e3d5d0bb754
coding-hui <wecoding@yeah.net>
2024-08-29 23:15:10 +0800

    refactor: refactor test suite for clarity and efficiency
    - Remove unused imports from test file
    - Remove a test function `testFindOriginalUpdateBlocks`
    - Simplify the test function `TestEditBlockCoder` by removing a sub-test
    
    Signed-off-by: coding-hui <wecoding@yeah.net>

--- internal/coders/diff_block_editor_test.go
+++ internal/coders/diff_block_editor_test.go
@@ -1,17 +1,14 @@
 package coders
 
 import (
-	"context"
 	"regexp"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 func TestEditBlockCoder(t *testing.T) {
 	t.Run("splitRawBlocks", testSplitRawBlocks)
-	t.Run("findOriginalUpdateBlocks", testFindOriginalUpdateBlocks)
 	t.Run("findFilename", testFindFilename)
 	t.Run("replaceMostSimilarChunk", testReplaceMostSimilarChunk)
 }
@@ -69,128 +66,6 @@ type Coder interface {
 	splitRe := regexp.MustCompile("(?m)^((?:" + separators + ")[ ]*\n)")
 	s := splitRe.Split(content, -1)
 	assert.Equal(t, len(s), 4)
-}
-
-func testFindOriginalUpdateBlocks(t *testing.T) {
-
-	text := `为了优化 supportCommands，我们需要为注册的方法添加 context.Context 参数。这将允许我们在这些方法中使用上下文进行取消操作或其他上下文相关的操作。
-
-以下是需要进行的更改：
-
-1. 修改 supportCommands 的定义，使其包含 context.Context 参数。
-2. 更新所有注册的方法，使其接受 context.Context 参数。
-3. 更新 run 方法，使其传递 context.Context 参数给调用的方法。
-
-以下是具体的更改步骤：
-
-<code>go
-internal\coders\commands.go
-<<<<<<< SEARCH
-var supportCommands = map[string]func(...string) tea.Msg{}
-=======
-var supportCommands = map[string]func(context.Context, ...string) tea.Msg{}
->>>>>>> REPLACE
-</code>
-
-<code>go
-internal\coders\commands.go
-<<<<<<< SEARCH
-func (c *command) registryCmds() {
-	supportCommands["/add"] = c.addFiles
-	supportCommands["/list"] = c.listFiles
-	supportCommands["/remove"] = c.removeFiles
-	supportCommands["/ask"] = c.askFiles
-	supportCommands["/drop"] = c.dropFiles
-	supportCommands["/coding"] = c.coding
-}
-=======
-func (c *command) registryCmds() {
-	supportCommands["/add"] = c.addFiles
-	supportCommands["/list"] = c.listFiles
-	supportCommands["/remove"] = c.removeFiles
-	supportCommands["/ask"] = c.askFiles
-	supportCommands["/drop"] = c.dropFiles
-	supportCommands["/coding"] = c.coding
-}
-
-func (c *command) addFiles(ctx context.Context, files ...string) tea.Msg {
-	return c.addFiles(files...)
-}
-
-func (c *command) listFiles(ctx context.Context, _ ...string) tea.Msg {
-	return c.listFiles()
-}
-
-func (c *command) removeFiles(ctx context.Context, files ...string) tea.Msg {
-	return c.removeFiles(files...)
-}
-
-func (c *command) askFiles(ctx context.Context, args ...string) tea.Msg {
-	return c.askFiles(args...)
-}
-
-func (c *command) dropFiles(ctx context.Context, _ ...string) tea.Msg {
-	return c.dropFiles()
-}
-
-func (c *command) coding(ctx context.Context, args ...string) tea.Msg {
-	return c.coding(args...)
-}
->>>>>>> REPLACE
-</code>
-
-<code>go
-internal\coders\commands.go
-<<<<<<< SEARCH
-func (c *command) run(input string) tea.Cmd {
-	return func() tea.Msg {
-		cmd, args := "/ask", []string{input}
-		if c.isCommand(input) {
-			cmd, args = extractCmdArgs(input)
-		}
-
-		cmdFunc, ok := supportCommands[cmd]
-		if !ok {
-			return c.coder.Errorf("Invalid command %s", cmd)
-		}
-
-		// do run
-		return cmdFunc(args...)
-	}
-}
-=======
-func (c *command) run(ctx context.Context, input string) tea.Cmd {
-	return func() tea.Msg {
-		cmd, args := "/ask", []string{input}
-		if c.isCommand(input) {
-			cmd, args = extractCmdArgs(input)
-		}
-
-		cmdFunc, ok := supportCommands[cmd]
-		if !ok {
-			return c.coder.Errorf("Invalid command %s", cmd)
-		}
-
-		// do run
-		return cmdFunc(ctx, args...)
-	}
-}
->>>>>>> REPLACE
-</code>
-
-这些更改将为 supportCommands 中的所有方法添加 context.Context 参数，并确保在调用这些方法时传递上下文。
-`
-	editor := NewEditBlockCoder(NewAutoCoder(), []string{"<code>", "</code>"})
-	editor.partialResponseContent = text
-
-	go func() {
-		<-editor.coder.checkpointChan
-	}()
-
-	edits, err := editor.GetEdits(context.Background())
-	require.NoError(t, err)
-	assert.NotEmpty(t, edits)
-	assert.Equal(t, 3, len(edits))
 }
 
 func testFindFilename(t *testing.T) {
