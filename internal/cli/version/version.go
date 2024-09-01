9e4d8af42fa67ea581a9b3488caadb2f58eded42
coding-hui <wecoding@yeah.net>
2024-09-01 16:10:10 +0800

    feat: enhance version command functionality
    - Add support for Go template in version command
    - Enhance version command to include more detailed version information
    - Add options to output version in JSON or YAML format
    - Improve error message for invalid output format
    
    Signed-off-by: coding-hui <wecoding@yeah.net>

--- internal/cli/version/version.go
+++ internal/cli/version/version.go
@@ -9,6 +9,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"text/template"
 
 	"github.com/ghodss/yaml"
 	"github.com/spf13/cobra"
@@ -22,13 +23,23 @@ import (
 
 var versionExample = templates.Examples(`
 		# Print the client and server versions for the current context
-		ai version`)
+		ai version
+
+		# Print the version in JSON format
+		ai version --output json
+
+		# Print the version in YAML format
+		ai version --output yaml
+
+		# Print the version using a custom Go template
+		ai version --template '{{.GitVersion}}'`)
 
 // Options is a struct to support version command.
 type Options struct {
 	ClientOnly bool
 	Short      bool
 	Output     string
+	Template   string
 
 	genericclioptions.IOStreams
 }
@@ -37,6 +48,7 @@ type Options struct {
 func NewOptions(ioStreams genericclioptions.IOStreams) *Options {
 	return &Options{
 		IOStreams: ioStreams,
+		Template:  "",
 	}
 }
 
@@ -44,9 +56,18 @@ func NewOptions(ioStreams genericclioptions.IOStreams) *Options {
 func NewCmdVersion(ioStreams genericclioptions.IOStreams) *cobra.Command {
 	o := NewOptions(ioStreams)
 	cmd := &cobra.Command{
-		Use:     "version",
-		Short:   "Print the cli version information",
-		Long:    "Print the cli version information for the current context",
+		Use:   "version",
+		Short: "Print the cli version information",
+		Long: `Print the cli version information for the current context.
+
+The version information includes the following fields:
+- GitVersion: The semantic version of the build.
+- GitCommit: The git commit hash of the build.
+- GitTreeState: The state of the git tree, either 'clean' or 'dirty'.
+- BuildDate: The date of the build.
+- GoVersion: The version of Go used to compile the binary.
+- Compiler: The compiler used to compile the binary.
+- Platform: The platform (OS/Architecture) for which the binary was built.`,
 		Example: versionExample,
 		Run: func(cmd *cobra.Command, args []string) {
 			util.CheckErr(o.Validate())
@@ -56,6 +77,7 @@ func NewCmdVersion(ioStreams genericclioptions.IOStreams) *cobra.Command {
 
 	cmd.Flags().BoolVar(&o.Short, "short", o.Short, "If true, print just the version number.")
 	cmd.Flags().StringVarP(&o.Output, "output", "", o.Output, "One of 'yaml' or 'json'.")
+	cmd.Flags().StringVarP(&o.Template, "template", "t", o.Template, "Template string to format the version output.")
 
 	return cmd
 }
@@ -63,7 +85,7 @@ func NewCmdVersion(ioStreams genericclioptions.IOStreams) *cobra.Command {
 // Validate validates the provided options.
 func (o *Options) Validate() error {
 	if o.Output != "" && o.Output != "yaml" && o.Output != "json" {
-		return errors.New(`--output must be 'yaml' or 'json'`)
+		return errors.New("Invalid output format. Please use 'yaml' or 'json'.")
 	}
 
 	return nil
@@ -74,31 +96,40 @@ func (o *Options) Run() error {
 	var serverErr error
 	versionInfo := version.Get()
 
-	switch o.Output {
-	case "":
-		if o.Short {
-			fmt.Fprintf(o.Out, "Version: %s\n", versionInfo.GitVersion)
-		} else {
-			fmt.Fprintf(o.Out, "Version: %s\n", fmt.Sprintf("%#v", versionInfo))
+	if o.Template != "" {
+		tmpl, err := template.New("version").Parse(o.Template)
+		if err != nil {
+			return fmt.Errorf("Failed to parse template: %v", err)
 		}
-	case "yaml":
-		marshaled, err := yaml.Marshal(&versionInfo)
+		err = tmpl.Execute(o.Out, versionInfo)
 		if err != nil {
 			return err
 		}
-
-		fmt.Fprintln(o.Out, string(marshaled))
-	case "json":
-		marshaled, err := json.MarshalIndent(&versionInfo, "", "  ")
-		if err != nil {
-			return err
+	} else {
+		switch o.Output {
+		case "":
+			if o.Short {
+				fmt.Fprintf(o.Out, "%s\n", versionInfo.GitVersion)
+			} else {
+				fmt.Fprintf(o.Out, "Version: %s\n", versionInfo.GitVersion)
+			}
+		case "yaml":
+			marshaled, err := yaml.Marshal(&versionInfo)
+			if err != nil {
+				return err
+			}
+			fmt.Fprintln(o.Out, string(marshaled))
+		case "json":
+			marshaled, err := json.MarshalIndent(&versionInfo, "", "  ")
+			if err != nil {
+				return err
+			}
+			fmt.Fprintln(o.Out, string(marshaled))
+		default:
+			// There is a bug in the program if we hit this case.
+			// However, we follow a policy of never panicking.
+			return fmt.Errorf("Invalid output format: %q. Please use 'yaml' or 'json'.", o.Output)
 		}
-
-		fmt.Fprintln(o.Out, string(marshaled))
-	default:
-		// There is a bug in the program if we hit this case.
-		// However, we follow a policy of never panicking.
-		return fmt.Errorf("VersionOptions were not validated: --output=%q should have been rejected", o.Output)
 	}
 
 	return serverErr
