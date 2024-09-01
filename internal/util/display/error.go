2b9a3008dc25853c1161fb89214badb03b21aea6
coding-hui <wecoding@yeah.net>
2024-09-01 21:55:09 +0800

    docs: improve error handling function descriptions
    - Enhance the description of the `Error` function
    - Improve the description of the `ErrorMsg` function
    - Clarify the description of the `FatalErr` function
    
    Signed-off-by: coding-hui <wecoding@yeah.net>

--- internal/util/display/error.go
+++ internal/util/display/error.go
@@ -12,7 +12,7 @@ var style = lipgloss.NewStyle().
 	PaddingTop(1).
 	Foreground(lipgloss.Color("9"))
 
-// Error handles and displays an error message along with optional additional messages, if any.
+// Error handles and displays an error message along with optional additional messages.
 func Error(err error, msgs ...string) {
 	if err == nil {
 		return
@@ -29,14 +29,14 @@ func Error(err error, msgs ...string) {
 	ErrorMsg(err.Error())
 }
 
-// ErrorMsg displays one or more error messages using a predefined style.
+// ErrorMsg displays one or more error messages using a predefined style for enhanced visibility.
 func ErrorMsg(msgs ...string) {
 	for _, msg := range msgs {
 		fmt.Println(style.Render(msg))
 	}
 }
 
-// FatalErr handles and displays an error message along with optional additional messages, if any, then exits the program with a status code of 1.
+// FatalErr handles and displays an error message along with optional additional messages, then exits the program with a status code of 1.
 func FatalErr(err error, msgs ...string) {
 	Error(err, msgs...)
 	os.Exit(1)
